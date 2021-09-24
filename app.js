ac19ccd3a2916569bacecce96b0bfa1f4f3d2779
APilganchuk <opilganchuk@gmail.com>
2021-09-24 17:35:03 +0300

    init

--- app.js
+++ app.js
@@ -2,4 +2,11 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (request, response) => {
+  response.send("Main");
+});
+app.get("/about", (request, response) => {
+  response.send(`${request.method}`);
+});
+
 app.listen(3000);
