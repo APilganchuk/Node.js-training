914443567dacaa0303137d92a20228ac6a68e675
APilganchuk <opilganchuk@gmail.com>
2021-09-24 16:48:00 +0300

    init

--- users.js
+++
@@ -1,9 +0,0 @@
-const admins = ["Mango", "Poly", "Kong"];
-
-const clients = ["Anna", "Mary", "Liza"];
-
-function getCurrentDay() {
-  return Date.now();
-}
-
-module.exports = { admins, clients, getCurrentDay };
