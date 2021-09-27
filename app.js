1ba708b6d2c9c594cc0c2c50eb6b9ac19f60a707
APilganchuk <opilganchuk@gmail.com>
2021-09-27 16:57:46 +0300

    progress

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
 const express = require("express");
+const fs = require("fs/promises");
+const moment = require("moment");
+const cors = require("cors");
+const contacts = require("./db/contacts.json");
 
 const app = express();
+app.use(cors());
 
-app.get("/", (request, response) => {
-  response.send("Main");
-});
-app.get("/about", (request, response) => {
-  response.send(`${request.method}`);
+app.use(async (req, res, next) => {
+  try {
+    const { method, url } = req;
+    const date = moment().format("DD.MM.YY._hh:mm:ss");
+    const text = `${method} ${url} ${date}`;
+    await fs.appendFile("db/server.log", `${text}\n`);
+    next();
+  } catch (error) {
+    console.log(error.message);
+    next();
+  }
 });
 
+app.get("/contacts", (req, res) => {
+  res.json(contacts);
+});
+
+app.post("/contacts", (req, res) => {
+  res.json({
+    status: "success",
+    message: `Contacts add `,
+  });
+});
+app.use((req, res) => {
+  res.status(404).json({
+    status: "error",
+    code: 404,
+    message: "Not found",
+  });
+});
 app.listen(3000);
